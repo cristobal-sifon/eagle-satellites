99d208a2f0733c12b16a24b4a0ef4549e0c82a2f
Cristobal Sifon <sifon@astro.princeton.edu>
2022-06-15 21:42:43 +0100

    multi-panel shmr history

--- hbt/shmr_history.py
+++ hbt/shmr_history.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import sys
 from time import time
+from tqdm import tqdm
 import warnings
 
 from plottery.plotutils import colorscale, savefig, update_rcParams
@@ -22,10 +23,19 @@ from hbtpy.track import Track, HaloTracks
 
 warnings.simplefilter('ignore', RuntimeWarning)
 
-def main(debug=True):
+def main():
 
+    checkpoints = ['birth', 'sat', 'first_infall', 'last_infall', 'today']
+    # starts = _checkpoints[:-1]
+    # ends = _checkpoints[1:]
+    # if len(args.checkpoints) == 0:
+    #     checkpoints = [(s, e) for i, e in enumerate(ends, 1) for s in starts[:i]]
+    # else:
+    #     checkpoints = [(start,end) for start, end
+    #                    in zip(args.checkpoints[:-1], args.checkpoints[1:])]
     args = hbt_tools.parse_args(
-        args=[('--halo', {'default': None, 'type': int}),
+        args=[('--checkpoints', {'nargs': '*', 'default': checkpoints}),
+              ('--halo', {'default': None, 'type': int}),
               ('--minmass', {'default': 5e13, 'type': float}),
               ('--nhalos', {'default': 0, 'type': int}),
               ('--nsub', {'default': 50, 'type': int}),
@@ -34,16 +44,12 @@ def main(debug=True):
               ])
     np.random.seed(args.seed)
     sim = Simulation(args.simulation)
-
-    to = time()
     reader = HBTReader(sim.path)
-    print('Loaded reader in {0:.1f} seconds'.format(time()-to))
 
     isnap = -1
     # min masses are the same as in mass_relations.py
     subs = Subhalos(
-        reader.LoadSubhalos(isnap), sim, isnap, logMmin=8,
-        logM200Mean_min=9)
+        reader.LoadSubhalos(isnap), sim, isnap, logMmin=8, logM200Mean_min=9)
     print('Loaded subhalos!')
 
     shmr_history(args, subs)
@@ -54,8 +60,6 @@ def shmr_history(args, subs):
     sats = subs.satellites
     cens = subs.centrals
     jsort = np.argsort(cens['M200Mean']).values
-    starts = ('birth', 'sat', 'first_infall', 'last_infall')
-    ends = ('sat', 'first_infall', 'last_infall', 'today')
     if args.test:
         j = jsort[-5]
         halo_shmr_evolution(
@@ -70,32 +74,27 @@ def shmr_history(args, subs):
         jarr = jsort[j]
         if args.nhalos > 0:
             jarr = np.random.choice(jarr, args.nhalos, replace=False)
+    print(f'Total {jarr.size} halos with M200Mean > {args.minmass}')
+    return
     for j in jarr:
         print()
-        logm = np.log10(cens['M200Mean'].iloc[j])
+        mhost = cens['M200Mean'].iloc[j]
         trackid = cens['TrackId'].iloc[j]
-        print(f'HostHalo TrackID {trackid} (logM={logm:.2f})')
-        for i, start in enumerate(starts):
-            _ = [halo_shmr_evolution(
-                    args, subs, subs.sim, cens['HostHaloId'].iloc[j],
-                    n=args.nsub, ncores=args.ncores, start=start, end=end,
-                    hostmass=cens['M200Mean'].iloc[j])
-                 for end in ends[i:]]
+        hostid = cens['HostHaloId'].iloc[j]
+        print(f'HostHaloID {hostid} / TrackID {trackid} (logM/Msun={np.log10(mhost):.2f})')
+        halo_shmr_evolution(
+            args, subs, subs.sim, hostid,
+            n=args.nsub, ncores=args.ncores, hostmass=mhost)
     return
 
 
 def halo_shmr_evolution(args, subs, sim, host_halo_id, seed=None, n=30,
-                        cmap='inferno_r', vmin=0, vmax=14,
-                        ncores=10, start='birth', end='today', hostmass=None,
+                        cmap='inferno_r', vmin=0, vmax=13.8,
+                        ncores=10, hostmass=None,
                         show_shsmr=True, show_chsmr=True, show_central=True,
                         draw_constant_ratios=True):
-    """
-    For now, shmr should be the output of either of `Subhalos.hsmr` or
-    `Subhalos.shmr`
-    """
-    _checkpoints = ['birth', 'cent', 'sat', 'first_infall', 'last_infall',
-                    'today']
     ic()
+    # this is used to register expected order of things
     assert (n > 0) or (seed is not None and seed >= 0), \
         'either `n` or `seed` must be specified and greater than zero'
     ic(host_halo_id)
@@ -117,14 +116,15 @@ def halo_shmr_evolution(args, subs, sim, host_halo_id, seed=None, n=30,
     if vmax is None:
         vmax = sim.cosmology.age(sim.redshifts[-1]).value
     ic(vmin, vmax)
-    output = define_output(args, halo, start, end)
-    ic(output)
+    # output = define_output(args, halo, start, end)
+    # ic(output)
     jsat = select_satellites(args, sats['MboundType4'].values)
     nsat = (sats['MboundType4'] > 0)
     print(f'{c}: Selected {jsat.size}/{nsat.sum()} satellites')
     ic(jsat)
     ti = time()
-    load_kwargs = dict(vmin=vmin, vmax=vmax, start=start, end=end, cmap=cmap)
+    load_kwargs = dict(
+        vmin=vmin, vmax=vmax, checkpoints=args.checkpoints, cmap=cmap)
     if ncores > 1:
         with Pool(ncores) as pool:
             out = [pool.apply_async(
@@ -133,107 +133,102 @@ def halo_shmr_evolution(args, subs, sim, host_halo_id, seed=None, n=30,
                    for j in jsat]
             pool.close()
             pool.join()
-        out = [i.get() for i in out]
-        xy = [i for i in out if i is not None and i[0].size > 0]
+        xy = [i.get() for i in out]
     else:
-        xy_infall, xy_present = [], []
+        xy = []
         for i, j in enumerate(jsat):
             if i % 10 == 0:
                 print(f'i={i:2d}, jsat={j:5d}...')
             trackid = sats['TrackId'].iloc[j]
-            xy = load_shmr_track(trackid, sim, **load_kwargs)
-            if xy is None:
-                continue
-            xy_infall.append(xy[0])
-            xy_present.append(xy[1])
-    print(f'{c}: Loaded tracks in {time()-ti:.2f} s')
-    # plot
-    fig, ax = plt.subplots(figsize=(7,6))
-    # plot overall SHMR for reference
-    if show_chsmr:
-        kw = dict(sample='centrals', ls='-.', color='k', lw=1,
-                  hostmass='M200Mean', zorder=10,
-                  bins=np.logspace(8, 12, 9), min_hostmass=1e9,)
-        subs.hsmr(ax=ax, **kw)
-        #subs.hsmr(ax=ax_present, **kw)
-    if show_shsmr:
-        kw = dict(sample='satellites', ls='-', color='k', lw=1,
-                  hostmass='M200Mean', zorder=10,
-                  bins=np.logspace(8, 12, 9), min_hostmass=1e13)
-        subs.hsmr(ax=ax, **kw)
-    colors, cmap = colorscale(n=cent.z.size, vmin=vmin, vmax=vmax, cmap=cmap)
-    # not plotting the central subhalo
+            xy.append(load_shmr_track(trackid, sim, **load_kwargs))
+    xy = [xy_i for xy_i in xy if xy_i is not None] # and xy_i[0] > 0]
+    ic(len(xy), len(xy[0]), len(xy[0][0]))
     if show_central:
-        load_kwargs['start'] = 'birth'
-        load_kwargs['end'] = 'today'
-        xy_c = load_shmr_track(c, sim, **load_kwargs)
+        load_kwargs['checkpoints'] = ('birth', 'today')
+        # the [0] is needed because load_shmr_track now always returns an array of histories
+        xy_c = load_shmr_track(c, sim, **load_kwargs)[0]
+    else:
+        xy_c = None
+    ic(len(xy_c))
+    print(f'{c}: Loaded tracks in {time()-ti:.2f} s')
+    # plot preamble
+    colors, cmap = colorscale(n=cent.z.size, vmin=vmin, vmax=vmax, cmap=cmap)
+    annot_kwargs = dict(
+        xy=(0.05,0.95), xycoords='axes fraction', ha='left', va='top',
+        fontsize=18)
+    if hostmass is not None:
+        logm = np.log10(hostmass)
+        mlabel = rf'$\log\,M_\mathrm{{200m}}/\mathrm{{M}}_\odot = {logm:.2f}$'
+    ylim = (3e9,2e14) if show_central else (3e9,1e14)
+    ncols = len(xy[0])
+    ic(ncols)
+    # plot
+    fig, axes = plt.subplots(1, ncols, figsize=(6.5*ncols,6), constrained_layout=True)
+    #for i, (ax, xy_i) in enumerate(zip(axes, xy)):
+    for i, ax in enumerate(axes):
+        # plot overall SHMR for reference
+        if show_chsmr:
+            kw = dict(sample='centrals', ls='-.', color='k', lw=1,
+                    hostmass='M200Mean', zorder=10,
+                    bins=np.logspace(8, 12, 9), min_hostmass=1e9,)
+            subs.hsmr(ax=ax, **kw)
+        if show_shsmr:
+            kw = dict(sample='satellites', ls='-', color='k', lw=1,
+                    hostmass='M200Mean', zorder=10,
+                    bins=np.logspace(8, 12, 9), min_hostmass=1e13)
+            subs.hsmr(ax=ax, **kw)
+        # central subhalo
         if xy_c is not None:
             plot_shmr_track(ax, *xy_c, is_central=True, size=10)
-    # plot subhalos
-    _ = [plot_shmr_track(ax, x, y, colors=c) for x, y, c in xy]
+        # plot subhalos
+        _ = [plot_shmr_track(ax, xy_i[i][0], xy_i[i][1], colors=xy_i[i][2])
+             for xy_i in xy]
+        ic(args.checkpoints, i, args.checkpoints[i:i+2])
+        start, end = [chkp.replace('_', ' ') for chkp in args.checkpoints[i:i+2]]
+        text = fr'{start} $\rightarrow$ {end}'
+        if hostmass is not None:
+            text = '\n'.join([mlabel, text])
+        ax.annotate(text, **annot_kwargs)
+        ax.set(xlabel='$m_{\u2605}$'+r' $(\mathrm{M}_\odot)$',
+            xlim=(1e8,1.5e12), ylim=ylim, xscale='log', yscale='log')
+        ax.tick_params(axis='x', which='major', pad=10)
+    axes[0].set(ylabel=r'$m_\mathrm{sub}\,(\mathrm{M}_\odot)$')
+    for ax in axes[1:]:
+        ax.set_yticklabels([])
+    axes[0].annotate(
+        f'HostHaloId={host_halo_id}', xy=(0.95,0.05), xycoords='axes fraction',
+        ha='right', va='bottom', fontsize=14, fontfamily='monospace')
     if draw_constant_ratios:
         rc = '0.5'
-        x = np.logspace(8, 12, 10)
+        x = np.logspace(8, 12.3, 10)
         for r in np.logspace(0, 3, 4):
-            line = ax.plot(x, r*x, rc, lw=1, dashes=(6,4))
             if r < 0:
                 msg = fr'$m_\mathrm{{sub}}/'+'m_\u2605'+f'={r:.1f}$'
             else:
                 msg = fr'$m_\mathrm{{sub}}/'+'m_\u2605'+f'={r:.0f}$'
+            for ax in axes:
+                line = ax.plot(x, r*x, rc, lw=1, dashes=(6,4))
                 if r in (1, 1000):
                     ax.text(
-                        5e11/r**0.5, 0.70*r**0.5*5e11, msg,
-                        va='top', ha='right',
-                        color=rc, rotation=40, fontsize=12)
-    annot_kwargs = dict(
-        xy=(0.05,0.95), xycoords='axes fraction', ha='left', va='top',
-        fontsize=18)
-    #chkp = checkpoint.replace('_', ' ')
-    start, end = [i.replace('_', ' ') for i in (start, end)]
-    if hostmass is not None:
-        logm = np.log10(hostmass)
-        mlabel = rf'$\log\,M_\mathrm{{200m}}/\mathrm{{M}}_\odot = {logm:.2f}$'
-    #ax_infall.annotate(
-        ##r'$t_\mathrm{birth}\,\rightarrow\,t_\mathrm{infall}$', **annot_kwargs)
-        #f'{mlabel}' + '\n' + fr'birth $\rightarrow$ {chkp}', **annot_kwargs)
-    #ax_present.annotate(
-        ##r'$t_\mathrm{infall}\,\rightarrow\,$today', **annot_kwargs)
-        #f'{mlabel}' + '\n' + fr'{chkp} $\rightarrow$ today', **annot_kwargs)
-    if hostmass is not None:
-        ax.annotate(
-            #f'{mlabel}\n' + fr'{start} $\rightarrow$ {end}',
-                #f'Halo ID {},
-            '\n'.join([f'{mlabel}', fr'{start} $\rightarrow$ {end}']),
-            **annot_kwargs)
-    ylim = (3e9,2e14) if show_central else (3e9,1e14)
+                        5e11/r**0.5, 0.70*r**0.5*5e11, msg, va='top',
+                        ha='right', color=rc, rotation=40, fontsize=12)
     #for ax in (ax_infall, ax_present):
-    plt.colorbar(cmap, ax=ax, label='Universe age (Gyr)')
-    ax.set_xlabel('$m_{\u2605}$'+r' $(\mathrm{M}_\odot)$')
-    ax.set_ylabel(r'$m_\mathrm{sub}\,(\mathrm{M}_\odot)$')
-    ax.set(xlim=(1e8,1e12), ylim=ylim, xscale='log', yscale='log')
-    # for fig, output in zip((fig_infall,fig_present), outputs):
-    #     save_plot(fig, output, sim)
-    save_plot(fig, output, sim)
+    plt.colorbar(cmap, ax=axes, label='Universe age (Gyr)', pad=0.01)
+    output = define_output(args, halo)
+    save_plot(fig, output, sim, tight=False)
     return
 
 
 def load_shmr_track(trackid, sim, mkey='Mbound', vmin=0, vmax=14,
-                    start='birth', end='today', cmap='viridis'):
+                    checkpoints=('birth', 'today'), cmap='viridis'):
     """
-    start, end must any valid checkpoint:
+    checkpoints must any set of valid checkpoint:
         ('birth', 'cent', 'sat', 'first_infall', 'last_infall', 'today'))
     """
-    _checkpoints = [None, 'birth', 'cent', 'sat', 'first_infall',
-                    'last_infall', 'today']
-    assert start in _checkpoints, f'start must be one of {_checkpoints}'
-    assert end in _checkpoints, f'end must be one of {_checkpoints}'
     # note that in the current implementation it is possible to have
     # t_cent > t_sat for a given subhalo
-    assert not (start == 'cent' and end == 'sat'), \
-        f'ambiguous start and end: {start} --> {end}'
-    assert _checkpoints.index(start) < _checkpoints.index(end) \
-            or end is None, \
-        f'start ({start}) must be an earlier checkpoint than end ({end})'
+    assert not np.in1d(checkpoints, ['cent','sat']).sum() == 2, \
+        f'ambiguous checkpoint set (cent,sat)'
     track = Track(trackid, sim)
     colors, _ = colorscale(n=track.z.size, vmin=vmin, vmax=vmax, cmap=cmap)
     if mkey == 'Mbound':
@@ -242,23 +237,25 @@ def load_shmr_track(trackid, sim, mkey='Mbound', vmin=0, vmax=14,
         mass = track.track['M200Mean']
     x = track.MboundType[:,4]
     y = mass
-    if start is None or start == 'birth':
-        istart = 0
-    else:
-        istart = track.history(cols='isnap', when=start, raise_error=False)
-        if istart is None:
-            istart = -1
-    if end is None or end == 'today':
-        iend = None
-    else:
-        iend = track.history(cols='isnap', when=end, raise_error=False)
-        if iend == istart:
-            iend += 1
-    ic(trackid, start, end, istart, iend)
-    #if istart is None or iend is None:
-        #return None
-    j = np.s_[istart:iend]
-    return x[j], y[j], colors[j]
+    isnaps = []
+    for i, chkp in enumerate(checkpoints):
+        if chkp == 'birth':
+            isnaps.append(0)
+            continue
+        if chkp == 'today':
+            isnaps.append(-1)
+            continue
+        i_ = track.history(cols='isnap', when=chkp, raise_error=False)
+        isnaps.append(-1 if i_ is None else i_)
+        if i == 0: continue
+        if isnaps[-1] <= isnaps[-2]:
+            isnaps[-1] = isnaps[-2] + 1
+    ic(trackid, checkpoints, isnaps)
+    data = []
+    for i, isnap in enumerate(isnaps[1:]):
+        rng = np.s_[isnaps[i]:isnap]
+        data.append([x[rng], y[rng], colors[rng]])
+    return data
 
 
 def plot_shmr_track(ax, x, y, colors='k', size=6,
@@ -269,6 +266,8 @@ def plot_shmr_track(ax, x, y, colors='k', size=6,
     segments = np.transpose([
         np.array([x[:-1], x[1:]]), np.array([y[:-1], y[1:]])], axes=(2,1,0))
     ic(segments.shape)
+    if len(colors) == 0:
+        return
     lines = LineCollection(segments, colors=colors[1:])
     #ic(lines)
     ax.add_collection(lines)
@@ -281,7 +280,7 @@ def plot_shmr_track(ax, x, y, colors='k', size=6,
         mfc='w' if is_central else colors[-1], zorder=100)
     return
 
-def define_output(args, halo, start, end):
+def define_output(args, halo):
     mass_bins = (13, 13.3, 13.7, 14, 14.3, 15)
     c = halo['TrackId'].loc[halo['Rank'] == 0].iloc[0]
     logm = np.log10(halo['M200Mean'].iloc[0])
@@ -289,9 +288,10 @@ def define_output(args, halo, start, end):
     jbin = np.digitize(logm, mass_bins)
     mbins_label = [f'{i:.1f}'.replace('.', 'p')
                    for i in mass_bins[jbin-1:jbin+1]]
+    suff = '__'.join(args.checkpoints)
     output = os.path.join(
         'shmr_history', f'logm_{mbins_label[0]}_{mbins_label[1]}',
-        f'shmr_track{c}__{start}__{end}')
+        f'shmr_track{c}__{suff}')
     return output
 
 
@@ -305,22 +305,24 @@ def select_satellites(args, Mstar, nbins=10, logmstar_min=8.5):
     ic(good.sum())
     if good.sum() < args.nsub:
         return jmass[good]
+    if nbins > args.nsub:
+        nbins = args.nsub
     n = args.nsub // nbins
-    ic(n)
+    ic(args.nsub, nbins, args.nsub // nbins)
     # the n most massive
     j = jmass[-n:]
     ic(j, logm[-n:])
-    # the 1.1 is to ensure that the last element can be included
-    mbins = np.linspace(logmstar_min, 1.1*logm[good].max(), nbins)
+    mbins = np.linspace(logmstar_min, logm[-n], nbins)
     ic(mbins)
-    for i in range(len(mbins)):
-        ji = (logm > mbins[i-1]) & (logm <= mbins[i])
-        ic(i, ji.sum())
+    # add a while to get to the desired nsub
+    for i_mbin in range(len(mbins)):
+        ji = (logm > mbins[i_mbin-1]) & (logm <= mbins[i_mbin])
+        ic(i_mbin, ji.sum())
         if ji.sum() == 0:
             continue
         ji = np.random.choice(jmass[ji], n, replace=False) if ji.sum() > n \
             else jmass[ji]
-        ic(mbins[i-1:i+1], np.log10(Mstar)[ji])
+        ic(mbins[i_mbin-1:i_mbin+1], np.log10(Mstar)[ji])
         ic(ji)
         j = np.append(j, ji)
     ic(j)
