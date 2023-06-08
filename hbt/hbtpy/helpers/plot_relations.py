fix bug when scaling masses to Msun units

hbt/hbtpy/helpers/plot_relations.py
@@ -165,6 +165,8 @@ def wrap_relations_distance(args, stat):
         # uncomment when testing
         # ycols = ['Mbound']
         # bincols = ['Mstar']
+        ycols = ['Mbound/Mstar']
+        bincols = ['Mstar']
         for ycol in ycols:
             if stat == 'mean' \
                     and ycol in ('Mbound/Mstar', f'{h}:Mbound/{h}:Mstar'):
@@ -527,9 +529,9 @@ def wrap_relations_time(args, stat, do_time_differences=False):
 def do_xbins(X, mask, xbins, xlim=None, xscale='log'):
     ic()
     ic(xlim)
-    mask = mask & np.isfinite(X)
     if xlim is not None:
         X = X[(xlim[0] <= X) & (X <= xlim[1])]
+    mask = mask & np.isfinite(X)
     ic(X.shape, mask.shape, mask.sum())
     if isinstance(xbins, int):
         #ic(X[mask], X[mask].shape)
@@ -570,9 +572,10 @@ def plot_segregation_literature(ax, xcol, ycol):
     ax.errorbar(
         xlit, ylit, (ylitlo,ylithi), fmt='o', ms=10, elinewidth=3,
         color='k', zorder=100,
-        label=r'Sifón+18 ($\log\langle' \
-            ' m_{\u2605}/\mathrm{M}_\odot' \
-            r' \rangle=10.1$)')
+        # label=r'Sifón+18 ($\log\langle' \
+        #     ' m_{\u2605}/\mathrm{M}_\odot' \
+        #     r' \rangle=10.1$)')
+        label=r'Sifón+18 (1.3)')
     # Kumar+22
     xlit = np.array([0.2, 0.38, 0.58, 0.78])
     if '/R200Mean' in xcol:
@@ -592,9 +595,10 @@ def plot_segregation_literature(ax, xcol, ycol):
     ax.errorbar(
         xlit, ylit, (ylitlo,ylithi), fmt='s', ms=8, elinewidth=3,
         color='k', mfc='w', mew=3, zorder=100,
-        label=r'Kumar+22 ($\log\langle' \
-            ' m_{\u2605}/\mathrm{M}_\odot' \
-            r' \rangle=10.5$)')
+        # label=r'Kumar+22 ($\log\langle' \
+        #     ' m_{\u2605}/\mathrm{M}_\odot' \
+        #     r' \rangle=10.5$)')
+        label=r'Kumar+22 (3.2)')
     return
 
 
@@ -1078,10 +1082,10 @@ def plot_relation(sim, subs, xcol='Mstar', ycol='Mbound',
             and ('Mstar' in ycol or ycol.count('Mbound') == 2) \
             and (xcol.split('/')[0][-1] not in '012') \
             and statistic == 'mean':
-        t = np.logspace(-1.6, -0.2, 10)
-        ax.plot(t, 100*t**(2/3), '-', color='0.5', lw=2)
-        ax.text(0.1, 30, r"$\propto (R/R_\mathrm{200m})^{2/3}$", va='center',
-                ha='center', color='0.5', rotation=45, fontsize=14)
+        t = np.logspace(-0.7, 0, 10)
+        ax.plot(t, 20*t**1.3, '-', color='0.5', lw=2)
+        ax.text(0.6, 8, r"$\propto (R/R_\mathrm{200m})^{1.3}$", va='center',
+                ha='center', color='0.5', rotation=60, fontsize=14)
 
     if literature:
         xcol_split = xcol.split(':')

hbt/hbtpy/subhalo.py
@@ -131,10 +131,6 @@ class BaseSubhalo(BaseDataSet):
         super(BaseSubhalo, self).__init__(catalog, as_dataframe=as_dataframe)
         BaseSimulation.__init__(self, sim)
         self.cosmology = self.sim.cosmology
-        #self.Mbound = 1e10 * self.catalog['Mbound']
-        #self.MboundType = 1e10 * self.catalog['MboundType']
-        #self.Nbound = self.catalog['Nbound']
-        #self.NboundType = self.catalog['NboundType']
         self.as_dataframe = as_dataframe
         self.pvref = pvref
         self._update_mass_columns()
@@ -144,11 +140,12 @@ class BaseSubhalo(BaseDataSet):
         if (isinstance(col, str) and col in cols):
             return self.catalog[col]
         # conveninence names
-        colmap = {'Mgas': 'MboundType0', 'Mdm': 'MboundType1',
-                  'Mstar': 'MboundType4', 'Ngas': 'NboundType0',
+        masscols = {'Mbound': 'Mbound', 'Mgas': 'MboundType0',
+                    'Mdm': 'MboundType1', 'Mstar': 'MboundType4'}
+        colmap = {'Ngas': 'NboundType0',
                   'Ndm': 'NboundType1', 'Nstar': 'NboundType4',
                   'R': 'ComovingMostBoundDistance'}
-        colmap = {**colmap, 
+        colmap = {**masscols, **colmap, 
                   # positions
                   **{i: f'ComovingMostBoundPosition{n}'
                      for n, i in enumerate('xyz')},
@@ -188,10 +185,12 @@ class BaseSubhalo(BaseDataSet):
                     'can only calculate difference of two columns,'
                     f' received {col}')
             return self.__getitem__(col[0]) - self.__getitem__(col[1])
+        #norm = 1e10 if col in masscols else 1
+        norm = 1
         if col in cols:
-            return self.catalog[col]
+            return norm * self.catalog[col]
         if col in colmap:
-            return self.catalog[colmap[col]]
+            return norm * self.catalog[colmap[col]]
         if col in ('Mtot', 'Mtotal'):
             return self._get_total_mass()
         raise KeyError(f'column {col} not found')
@@ -590,6 +589,7 @@ class Subhalos(BaseSubhalo):
         self.verbose_when_loading = verbose_when_loading
         self.exclude_non_FoF = exclude_non_FoF
         self.non_FoF = (self.catalog['HostHaloId'] == -1)
+        # correct if they are given in linear space
         if logMmin is not None and logMmin > 100:
             logMmin = np.log10(logMmin)
         self.logMmin = logMmin
@@ -1143,7 +1143,7 @@ class Subhalos(BaseSubhalo):
             if 'M200' in col or col == 'MVir':
                 # otherwise I think this happens twice?
                 if self.catalog[col].max() < 1e6:
-                    self.catalog[col] = 1e6 * self.catalog[col]
+                    self.catalog[col] = 1e10 * self.catalog[col]
         if verbose:
             print('Joined hosts in {0:.2f} s'.format(time()-to))
         del hosts
@@ -1186,6 +1186,9 @@ class Subhalos(BaseSubhalo):
             history.reset_index(), how='left', left_on='TrackId',
             right_on='history:TrackId')
         self._update_mass_columns()
+        ic()
+        ic(history[['history:sat:time','history:max_Mstar:time']])
+        ic(self.catalog[['TrackId','history:sat:time','history:max_Mstar:time']])
         return
 
     def siblings(self, trackid, return_value='index'):

hbt/store_subhalo_times.py
@@ -234,20 +234,16 @@ def main():
         history.loc[jinfall_first, 'trackids:TrackId_previous_host'] \
             = this['TrackId_cent'][jinfall_first]
 
-        #ic(np.unique(history['cent/isnap'], return_counts=True))
-        #ic(np.sort(history['sat/Mbound']))
-        #ic((history > -1).sum())
-        #ic(history.mean())
-        if (args.store or True) and ((i % 10 == 0) or (snap < 10)):
+        if args.store and ((i % 10 == 0) or (snap < 10)):
             store_h5(hdf, groups, names, history)
-            #store_npy(path, groups, names, history)
-            #break
         if args.test and i >= 3:
             break
 
     if args.test or args.debug:
         ics['masses']()
         ics['masses'](history.iloc[:20][cols['max_Mstar']])
+        ics['masses'](
+            np.histogram(history['max_Mstar:time'], np.arange(0, 15, 2))[0])
 
     print(f'Failed: {failed}, i.e., {len(failed)} times')
     if args.store or args.test:
@@ -267,6 +263,7 @@ def assign_masses(history, this, mask, group):
 def max_mass(history, this, events, cols, snap, ti, zi):
     ics['masses']()
     #ic(this)
+    ic(np.sort(events))
     for event in events:
         if event[:4] == 'max_':
             m = event[4:].split('/')
@@ -283,17 +280,15 @@ def max_mass(history, this, events, cols, snap, ti, zi):
             history.loc[gtr, cols[event][:3]] = [snap, ti, zi]
             history = assign_masses(history, this, gtr, event)
             if m in ('Mstar', 'Mbound/Mstar'):
-                ics['masses'](history.loc[gtr, cols[event]][:10], gtr.sum())
+                ics['masses'](snap, history.loc[gtr, cols[event]][:10], gtr.sum())
     return history
 
 
 def store_h5(hdf, groups, names, data):
     with h5py.File(hdf, 'w') as out:
         for group, grnames in zip(groups, names):
-            #ic(group)
             gr = out.create_group(group.replace('/', '-over-'))
             for name in grnames:
-                #ic(name)
                 col = f'{group}:{name}'
                 if group == 'max_Mstar':
                     ics['masses'](col, data[col].to_numpy())